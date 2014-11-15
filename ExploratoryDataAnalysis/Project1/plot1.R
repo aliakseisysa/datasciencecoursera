57794e60c17027b04b098bde5ac85c4952eeabf1
aliakseisysa <aliaksei.sysa@gmail.com>
2014-11-15 23:52:45 +0300

    Update plot1.R

--- ExploratoryDataAnalysis/Project1/plot1.R
+++ ExploratoryDataAnalysis/Project1/plot1.R
@@ -3,13 +3,13 @@ mydata <- read.csv("household_power_consumption.txt", header=T, sep=';', na.stri
 nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
 mydata$Date <- as.Date(mydata$Date, format="%d/%m/%Y")
 ## Subsetting the data
-data_sub <- subset(mydata, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+data <- subset(mydata, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
 rm(mydata)
 ## Converting dates
-datetime <- paste(as.Date(data_sub$Date), data_sub$Time)
-data_sub$Datetime <- as.POSIXct(datetime)
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
 ## Plot 1
-hist(data_sub$Global_active_power, main="Global Active Power",
+hist(data$Global_active_power, main="Global Active Power",
 xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
 ## Saving to file
 dev.copy(png, file="plot1.png", height=480, width=480)
